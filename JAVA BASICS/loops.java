edc0c431262252d0a071b07236fe17dafa4c0cb9
Kunal Kumar Vishwakarma <kunalvishwa123@gmail.com>
2026-08-08 18:10:14 +0530

    Add files via upload

---
+++ JAVA BASICS/loops.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+void main() {
+//    For Loop Q1 (Printing Number Series)
+//    int i;
+//    for (i=1; i<=10; i++){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q2 (Printing Reverse Number Series)
+//    for (i=10; i>0; i--){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q3 ( Printing Series Of Even Numbers )
+//    for (int i=2; i<=20; i++){
+//        if (i%2==0){
+//            System.out.println(i);
+//        }
+//    }
+
+//==============================================================
+
+//    For Loop Q4 ( Printing Series Of Odd Numbers )
+//    for (int i=1; i<=20; i++){
+//        if (i%2!=0){
+//            System.out.println(i);
+//        }
+//    }
+
+//==============================================================
+
+//    For Loop Q5 ( Printing Number From 50-100 )
+//    for (int i=50; i<=100; i++){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q6 ( Printing Number From 100-50 )
+//    for (int i=100; i>=50; i--){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q7 ( Printing String Series )
+//    for (int i=1; i<=5; i++){
+//        System.out.println("Welcome To Java");
+//    }
+
+//==============================================================
+
+//    For Loop Q8 ( Taking Input From User And Printing Number Series )
+//    Scanner sc = new Scanner(System.in);
+//    System.out.print("Enter A Number : ");
+//    int input = sc.nextInt();
+//    for (int i=1; i<=input; i++){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q9 ( Printing Number Series Multiple Of 5 )
+//    for (int i=5; i<=100; i+=5){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q10 ( Printing Number Series Number Divisible By 3 and 7 )
+//    for (i=1; i<=100; i++){
+//        if (i%3==0 && i%7==0){
+//            System.out.println(i);
+//        }
+//    }
+
+//==============================================================
+
+//    For Loop Q11 ( Taking Input From User Starting And Ending Values And Printing Num series )
+//    Scanner sc = new Scanner(System.in);
+//    int start_num,end_num,i;
+//    System.out.println("Enter Your Starting Number: ");
+//    start_num = sc.nextInt();
+//    System.out.println("Enter Your Ending Number: ");
+//    end_num = sc.nextInt();
+//
+//    for (i=start_num; i<=end_num; i++){
+//        System.out.println(i);
+//    }
+
+//==============================================================
+
+//    For Loop Q12 ( Printing Table of Any Number Given Input By User )
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter Your Desired Number : ");
+//    int num = sc.nextInt();
+//
+//    for (int i=1; i<=10; i++){
+//        System.out.println(num + " * " + i + " = " +num*i);
+//    }
+
+//==============================================================
+
+//    For Loop Q13 ( Printing Prime Number Till Range Given By User )
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter Starting Number : ");
+//    int start_num = sc.nextInt();
+//    System.out.println("Enter Ending Number : ");
+//    int end_num = sc.nextInt();
+//    for (int i=start_num; i<=end_num; i++){
+//        boolean isPrime = true;
+//        for (int j=2; j<i; j++){
+//            if (i%j==0){
+//                isPrime = false;
+//                break;
+//            }
+//        }
+//        if (isPrime && i>1){
+//            System.out.println(i);
+//        }
+//    }
+}
