392b8a54d272bd5141f242fbc87601541f4c05d1
Kunal Kumar Vishwakarma <kunalvishwa123@gmail.com>
2026-08-09 21:51:40 +0530

    Create while_loop.java

---
+++ JAVA BASICS/while_loop.java
@@ -0,0 +1,9 @@
+void main() {
+//    While Loop Q1 ( Printing Number Series From 1-10 )
+    int i = 1;
+    while(i <= 10){
+        System.out.println(i);
+        i++;
+    }
+
+//===============================================================
