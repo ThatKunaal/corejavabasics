1cea289e05260cc02c81b3870d2d52b05544eff7
Kunal Kumar Vishwakarma <kunalvishwa123@gmail.com>
2026-08-10 15:46:46 +0530

    Update while_loop.java

--- JAVA BASICS/while_loop.java
+++ JAVA BASICS/while_loop.java
@@ -111,3 +111,58 @@ void main() {
         num = num / 10;
     }
     System.out.print("Reverse = " + rev);    
+
+//===============================================================
+
+//    While Loop Q10 ( Palindrom Number Checker )
+    Scanner sc = new Scanner(System.in);
+    long num,digit,og,rev=0;
+    System.out.println("Enter Your Number To Check : ");
+    num = sc.nextLong();
+    og=num;
+
+    while(num>0){
+        digit = num%10;
+        rev = rev*10+digit;
+        num = num/10;
+    }
+    if (rev==og){
+        System.out.println("Palindrom");
+    }else {
+        System.out.println("NoT Palindrom");
+    }
+
+//===============================================================
+
+//    While Loop Q11 ( Sum Of Even Digit In A Number )
+   Scanner sc = new Scanner(System.in);
+   System.out.print("Enter Your Number : ");
+   long num = sc.nextLong();
+   long sum = 0;
+   while(num>0){
+       long digit = num%10;
+       num = num/10;
+       if(digit%2==0){
+           sum=sum+digit;
+       }
+   }
+   System.out.println("Sum of Even Is = "+sum);
+
+//===============================================================
+
+//    While Loop Q12 ( Count No Of Even & Odd Digits In A number )
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter Your Number : ");
+   long num = sc.nextLong();
+   long even_count=0,odd_count=0;
+   while(num>0){
+       float digit = num%10;
+       num = num/10;
+       if (digit%2==0 || digit==0){
+           even_count++;
+       }else {
+           odd_count++;
+       }
+   }
+   System.out.println("Even Count = "+even_count);
+   System.out.println("Odd Count = "+odd_count);
