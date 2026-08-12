a3490da7f6cd182d02035a60eb279fd59b8882dd
Kunal Kumar Vishwakarma <kunalvishwa123@gmail.com>
2026-08-12 23:10:43 +0530

    Create Switch Statements.java

---
+++ JAVA BASICS/Switch Statements.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+void main() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("======[ Welcome To The Admission Portal ]======");
+    System.out.println();
+    System.out.println("1.BTECH\n2.BCA\n3.BBA");
+    System.out.println();
+    System.out.println("===============================================");
+    System.out.print("CHOOSE THE COURSE YOU WANT TO PURSUE : ");
+    int choice = sc.nextInt();
+    System.out.println();
