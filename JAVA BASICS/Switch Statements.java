9ed73bea3b1fb6ec8f691a707ceab14c92f75947
Kunal Kumar Vishwakarma <kunalvishwa123@gmail.com>
2026-08-12 23:11:33 +0530

    Update Switch Statements.java

--- JAVA BASICS/Switch Statements.java
+++ JAVA BASICS/Switch Statements.java
@@ -28,6 +28,44 @@ void main() {
                     "\n" +
                     "Admission Status : Open\n" +
                     "==============================");
+             System.out.print("Do You Want To Enroll In This Course (Y/N) : ");
+            sc.nextLine();
+            String choice2 = sc.nextLine();
+            choice2 = choice2.toLowerCase();
 
-           
+            if (choice2.startsWith("y")){
+                System.out.println();
+                System.out.println("==================================================");
+                System.out.println("ENTER YOUR DETAILS");
+                System.out.print("Name : ");
+                String sname = sc.nextLine();
+                System.out.print("Fathers Name : ");
+                String fname = sc.nextLine();
+                System.out.print("Age : ");
+                int age = sc.nextInt();
+
+                System.out.print("DOB [dd/mm/yyyy] : ");
+                String dob = sc.next();
+
+                System.out.println();
+                System.out.print("Print Receipt (Y/N) : ");
+                String choice3 = sc.next();
+
+                if (choice3.equalsIgnoreCase("Y")) {
+                    System.out.println("============== RECEIPT ==============");
+                    System.out.println("Student Name : " + sname);
+                    System.out.println("Father Name  : " + fname);
+                    System.out.println("Age          : " + age);
+                    System.out.println("DOB          : " + dob);
+                    System.out.println("Receipt No.  : 23456");
+                    System.out.println("Course       : B.Tech");
+                    System.out.println("Batch        : 2024-2028");
+                    System.out.println("=====================================");
+                } else {
+                    System.out.println("!! Please Print The Receipt !!");
+                }
+            break;
+
+            }
+    }       
 }
